88eca231a079943180ebd6e0b19e8dd7e6e01628
passiondev0503 <passiondev0503@gmail.com>
2021-09-01 13:12:30 +0530

    example for getting rates of a hotel rate plan

---
+++ src/example/rates.js
@@ -0,0 +1,56 @@
+/**
+ * Apaleo 🦁 Rates Adapter javascript example
+ * 
+ */
+const { join } = require('path')
+const { config } = require('dotenv')
+const { ApaleoConnectAdaptor } = require('./../../dist/lib')
+// the above line equal to ->  const { ApaleoConnectAdaptor } = require('@cord-travel/pms-connect-apaleo)
+config({
+    path:join(__dirname, '../../.env')
+})
+
+
+
+
+
+const { APALEO_CLIENT_ID = "",
+    APALEO_CLIENT_SECRET = "",
+    APALEO_REDIRECT_URI,
+    TEST_APALEO_REFRESH_TOKEN,
+    TEST_APALEO_ACCESS_TOKEN } = process.env
+
+    
+const apaleo = new ApaleoConnectAdaptor({
+    client_id: APALEO_CLIENT_ID,
+    client_secret: APALEO_CLIENT_SECRET,
+    redirect_uri: APALEO_REDIRECT_URI,
+    refresh_token: TEST_APALEO_REFRESH_TOKEN,
+    access_token: TEST_APALEO_ACCESS_TOKEN
+
+})
+    
+
+async function main() {
+    const hotels = await apaleo.getHotels()
+    console.log(`Getting hotel id 🐹`)
+
+    console.log(`        hotel id : ${hotels.data[0].id} `)
+
+    console.log(`Getting hotel Rate plans of ${hotels.data[0].id}`)
+    const ratePlans = await apaleo.getRatePlansByHotelId(hotels.data[0].id)
+
+    console.log(`        First Rate Plan Id : ${ratePlans.data[0].id} `)
+
+    console.log(`Getting Rates of the rate plans ${ratePlans.data[0].id}`)
+    const rates = await apaleo.getRatesByRatePlan(hotels.data[0])
+    console.log(rates)
+
+    console.log(`🦁🦁🦁🦁🦁🦁🦁🦁🦁🦁🦁🦁🦁🦁🦁🦁🦁🦁`)
+
+
+}
+
+main().catch(error => {
+    console.log(` Error 👹 `, error)
+})
